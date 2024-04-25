2bad3f9d9b1e655467e0e68ae1cfacda2183c73d
Synzenn <velriel42@gmail.com>
2024-04-25 10:46:54 +0700

    menyelesaikan deletenood

--- LogikaSinglelinked/LogikaSinglelinked.cpp
+++ LogikaSinglelinked/LogikaSinglelinked.cpp
@@ -73,6 +73,9 @@ bool deleteNode(int nim) {
     if (searchNode(nim, previous, current) ==
         false)
         return false;
-   
+    previous->next = current->next;
+    if (current == START)
+        START = current->next;
+    return true;
 }
 
