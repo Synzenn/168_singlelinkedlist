0f0c9acf680f85381d237800032c9353241148f0
Synzenn <velriel42@gmail.com>
2024-04-25 10:46:41 +0700

    membuat loop if

--- LogikaSinglelinked/LogikaSinglelinked.cpp
+++ LogikaSinglelinked/LogikaSinglelinked.cpp
@@ -68,6 +68,11 @@ bool searchNode(int nim, Node* current, Node* previous) {
 }
 
 bool deleteNode(int nim) {
-    
+    Node* current = START;
+    Node* previous = START;
+    if (searchNode(nim, previous, current) ==
+        false)
+        return false;
+   
 }
 
